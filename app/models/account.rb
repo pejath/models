6696eddca84ee39e6f919a29ec7bcd8a709477d3
pejath <pejath@yandex.ru>
2022-01-04 08:50:08 +0300

    scope for accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,8 @@ class Account < ApplicationRecord
   belongs_to :person
   validates_format_of :email, with: /\A\w+@\w+/
   validates :password, length: {minimum: 8}, presence: true
+
+  scope :has_more_than, ->(num) {where(id: Purchase.group(:account_id).having("count('account_id')>=?", num).pluck(:account_id))}
+
+
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,4 +3,7 @@ class Purchase < ApplicationRecord
   belongs_to :product
   validates :delivert, inclusion: {in: [true, false]}
   validates :typeOfPayment, presence: true
+
+  # Purchase.group(:account_id).having("count('id')>=2").select(:account_id)
+
 end
