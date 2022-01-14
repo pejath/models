5921dcca270d982fde1020cf1b699c658bdb5a92
pejath <pejath@yandex.ru>
2022-01-14 11:21:35 +0300

    puts for main

--- main.rb
+++ main.rb
@@ -1,15 +1,16 @@
-#
+puts "Accounts that has more than 2 purchases"
 ap Account.has_more_than(2).select(:nickname, :email, "count('id') as Purchases")
   "-"*200
-
+puts "Cars that have been on sale for more than six months"
 ap Product.joins(:tech_info).long_on_sale.select(:id, 'carBrand AS Brand', :color, :price,
                                                             'tech_infos.id AS "Technical information №"', 'wos AS "went on sale"')
 puts "-"*200
+puts "New cars"
 ap Product.joins(:tech_info).new_auto.select(:id, 'car_brand AS Brand', :color, :price,
                                              'tech_infos.id AS "Technical information №"', 'wos AS "went on sale"')
 puts "-"*200
+
+puts "Volkswagen brand cars"
 ap Product.joins(:tech_info).brand("Volkswagen").select(:id, 'car_brand AS Brand', :color, :price,
                                                                  'tech_infos.id AS "Technical information №"', 'wos AS "went on sale"')
-puts "-"*200
-ap TechInfo.select(:id, :product_id, "body_type as 'Body Type'", "doors_num as 'Number of doors'",
-                   "seats_num as ' Number of Seats'", "engine_type as 'Engine Type'", "engine_displ as 'Engine Displacement'").order(:product_id)
+
